068683a108afa4caf6a820631f4d99466937a82b
Andrew Bowley <andrewbowley@aapt.net.au>
2014-11-04 11:46:13 +1100

    Add set up Eclipse

---
+++ install/setup-eclipse.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+set TYPE=maven
+if "%1"=="" goto start_op
+set TYPE=classic
+:start_op
+@echo classyjava 
+pushd ..\classyjava
+xcopy /Y eclipse\%TYPE%\project .\.project
+xcopy /Y eclipse\%TYPE%\.classpath .
+popd
+@echo classyandroid
+pushd ..\classyfy\classyandroid
+xcopy /Y eclipse\%TYPE%\project .\.project
+xcopy /Y eclipse\%TYPE%\.classpath .
+popd
+@echo many2many-example
+pushd ..\classyfy\many2many-example
+xcopy /Y eclipse\%TYPE%\project .\.project
+xcopy /Y eclipse\%TYPE%\.classpath .
+popd
